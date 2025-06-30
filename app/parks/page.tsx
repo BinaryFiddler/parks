8768e875540774f8297cae4b31ef2e9484b831fc
Chenyu Huang <itschenyu@gmail.com>
2025-06-30 14:05:51 -0700

    reset default layout

--- app/parks/page.tsx
+++ app/parks/page.tsx
@@ -2,21 +2,19 @@ import '@root/global.scss';
 
 import ParkCard from '@components/ParkCard';
 import Grid from '@components/Grid';
-import Row from '@components/Row';
 import DefaultLayout from '@components/page/DefaultLayout';
 import { PARKS } from '@data/parks';
+import DefaultActionBar from '@root/components/page/DefaultActionBar';
 
 export const dynamic = 'force-static';
 
 export default function ParksPage() {
   return (
-    <DefaultLayout previewPixelSRC="/template-app-icon.png">
+    <DefaultLayout>
+      <DefaultActionBar />
       <Grid>
-        <Row>
-          <h1>U.S. National Parks</h1>
-        </Row>
-      </Grid>
-      <Grid>
+        <h1>U.S. National Parks</h1>
+
         {PARKS.map((park) => (
           <ParkCard key={park.id} park={park} href={`/parks/${park.id}`} />
         ))}
--- components/page/DefaultLayout.tsx
+++ components/page/DefaultLayout.tsx
@@ -3,17 +3,12 @@ import styles from '@components/page/DefaultLayout.module.scss';
 import * as React from 'react';
 
 interface DefaultLayoutProps {
-  previewPixelSRC: string;
+  previewPixelSRC?: string;
   children?: React.ReactNode;
 }
 
 const DefaultLayout: React.FC<DefaultLayoutProps> = ({ previewPixelSRC, children }) => {
-  return (
-    <div className={styles.body}>
-      <img className={styles.pixel} src={previewPixelSRC} alt="" />
-      {children}
-    </div>
-  );
+  return <div className={styles.body}>{children}</div>;
 };
 
 export default DefaultLayout;
